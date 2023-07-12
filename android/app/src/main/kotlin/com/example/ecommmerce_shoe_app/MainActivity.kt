c0dc8a29a1eb227d5556d8311a4c379b4063988c
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2023-07-12 14:49:07 +0530

    added base project for app

---
+++ android/app/src/main/kotlin/com/example/ecommmerce_shoe_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ecommmerce_shoe_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:ecommmerce_shoe_app/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Shoe App',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
