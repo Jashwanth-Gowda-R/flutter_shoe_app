da8c4cc7e0dc97d6534809a88b4ea72cf9a4acb7
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2023-07-13 22:56:08 +0530

    Added chips for brands

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<String> filters = const ['All', 'Addidas', 'Nike', 'Bata'];
+
+  late String selectedFilter;
+  @override
+  void initState() {
+    super.initState();
+    selectedFilter = filters[0];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,11 +29,11 @@ class HomePage extends StatelessWidget {
       ),
     );
 
-    return const Scaffold(
+    return Scaffold(
         body: SafeArea(
       child: Column(
         children: [
-          Row(
+          const Row(
             children: [
               Padding(
                 padding: EdgeInsets.all(20.0),
@@ -45,6 +59,49 @@ class HomePage extends StatelessWidget {
                 ),
               ),
             ],
+          ),
+          SizedBox(
+            height: 120,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: filters.length,
+              itemBuilder: (context, index) {
+                var fliterName = filters[index];
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 8,
+                  ),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedFilter = fliterName;
+                      });
+                    },
+                    child: Chip(
+                      label: Text(
+                        fliterName,
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 15,
+                      ),
+                      backgroundColor: selectedFilter == fliterName
+                          ? Theme.of(context).colorScheme.primary
+                          : const Color.fromRGBO(245, 247, 249, 1),
+                      side: const BorderSide(
+                        color: Color.fromRGBO(245, 247, 249, 1),
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
           ),
         ],
       ),
